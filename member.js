fde03c096b91c076696c48ed9cf8b2b8f5742cf9
Scott Miller <scott_millers@hotmail.com>
2023-08-10 23:23:02 +0000

    Update

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var projectsBtn = document.getElementById("projectsBtn");
+    var contactBtn = document.getElementById("contactBtn");
+    var aboutBtn = document.getElementById("aboutBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberBtn.style.backgroundColor = "#f5f5f5";
+    skillsBtn.style.backgroundColor = "#ffffff";
+    projectsBtn.style.backgroundColor = "#ffffff";
+    contactBtn.style.backgroundColor = "#ffffff";
+    aboutBtn.style.backgroundColor = "#ffffff";
+} 
